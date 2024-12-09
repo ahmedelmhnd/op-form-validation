402478e181556b238e51a84530e2683b6ac88081
Ahmed A <ahmed.elmohanned@gmail.com>
2024-12-09 23:53:50 +0800

    completed country and email validation using js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
-const cat = "cat";
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span");
 
-for (let index = 0; index < 10; index++) {
-  console.log(cat);
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.classList = "error";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!(email.validity.valid && country.validity.valid)) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Please enter an e-mail address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "The value you enter has to be an e-mail address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  if (country.validity.valueMissing) {
+    countryError.textContent = "please enter a country"
+  } else if (country.validity.tooShort) {
+    countryError.textContent = `Email should be at least ${country.minLength} characters; you entered ${country.value.length}.`;
+  }
+
+  emailError.className = "error active";
+  countryError.className = "error active"
 }
+
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span");
+
+country.addEventListener("input", () => {
+  if (country.validity.valid) {
+    emailError.textContent = "";
+    emailError.classList = "error";
+  } else {
+    showError();
+  }
+});
